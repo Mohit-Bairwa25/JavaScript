0a15b0013a435eddec383d02db4eeae81a5fc446
Mohit-Bairwa25 <mohit.bairwa25@gmail.com>
2024-06-12 09:34:36 +0530

    Changes

---
+++ Math.js
@@ -0,0 +1,50 @@
+// Define a number
+let num = 10.49;
+
+// Math.abs(): returns the absolute value of a number
+console.log(Math.abs(-7.25)); // 7.25
+
+// Math.ceil(): rounds a number UPWARDS to the nearest integer
+console.log(Math.ceil(num)); // 11
+
+// Math.floor(): rounds a number DOWNWARDS to the nearest integer
+console.log(Math.floor(num)); // 10
+
+// Math.round(): rounds a number to the nearest integer
+console.log(Math.round(num)); // 10
+
+// Math.sqrt(): returns the square root of a number
+console.log(Math.sqrt(64)); // 8
+
+// Math.cbrt(): returns the cube root of a number
+console.log(Math.cbrt(27)); // 3
+
+// Math.random(): returns a random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random()); 
+
+// Math.max(): returns the number with the highest value
+console.log(Math.max(0, 150, 30, 20, -8, -200)); // 150
+
+// Math.min(): returns the number with the lowest value
+console.log(Math.min(0, 150, 30, 20, -8, -200)); // -200
+
+// Math.pow(): returns the value of x to the power of y
+console.log(Math.pow(8, 2)); // 64
+
+// Math.log(): returns the natural logarithm (base E) of a number
+console.log(Math.log(1)); // 0
+
+// Math.exp(): returns E^x, where x is the argument, and E is Euler's number (approximately 2.7183), the base of the natural logarithm
+console.log(Math.exp(1)); // 2.718281828459045
+
+// Math.sin(): returns the sine of a number
+console.log(Math.sin(90 * Math.PI / 180)); // 1
+
+// Math.cos(): returns the cosine of a number
+console.log(Math.cos(0)); // 1
+
+// Math.tan(): returns the tangent of an angle
+console.log(Math.tan(45 * Math.PI / 180)); // 0.9999999999999999
+
+// Math.PI: returns the ratio of the circumference of a circle to its diameter
+console.log(Math.PI); // 3.141592653589793
